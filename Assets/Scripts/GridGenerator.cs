5b2a75124ea1c8ddac4aa3f3d1d395227baf143d
wesleyallanrocha <wesleyallanrocha@gmail.com>
2018-01-24 00:44:54 -0200

    Começando a criar os polígonos;

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridGenerator : MonoBehaviour
+{
+    public int xSize, ySize;
+    private Vector3[] vertices;
+    private int[] triangles;
+    private Mesh mesh;
+
+    void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        Gizmos.color = Color.black;
+        foreach (Vector3 vertex in vertices)
+        {
+            Gizmos.DrawSphere(vertex, 0.1f);
+        }
+    }
+}
